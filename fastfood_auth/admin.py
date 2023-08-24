56eece68a8b4cef9d09d1374edf94f185b9d2030
Mathew Koech <123968710+Mathewkoech@users.noreply.github.com>
2023-08-24 12:20:47 +0300

    Delete fastfood_auth directory

--- fastfood_auth/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from fastfood_auth.models import User
-from django.contrib.auth.models import Permission
-
-
-admin.site.register(User)
-admin.site.register(Permission)
--- fastfood_auth/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FastfoodAuthConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fastfood_auth'
